1bb9ffc23be9bece0c70925a69f339b8c8d7203e
Anna Samsonenko <foster.anna93@gmail.com>
2021-12-03 11:00:19 +0200

    add hbs templates and js-files

---
+++ src/js/common/keys.js
@@ -0,0 +1,4 @@
+export const API_KEY = '8467b35301383f6844ef88f323a204bb';
+export const STORAGE_MAIN_KEY = 'mainItems';
+export const STORAGE_WATCHED_KEY = 'watchedMovies';
+export const STORAGE_QUEUE_KEY = 'queueMovies';
