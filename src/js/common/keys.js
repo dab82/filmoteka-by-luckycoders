88752a41c2402b2ba06b377f86f3dd74653821dc
Anna Samsonenko <foster.anna93@gmail.com>
2021-12-03 21:43:43 +0200

    add key

--- src/js/common/keys.js
+++ src/js/common/keys.js
@@ -2,3 +2,4 @@ export const API_KEY = '8467b35301383f6844ef88f323a204bb';
 export const STORAGE_MAIN_KEY = 'mainItems';
 export const STORAGE_WATCHED_KEY = 'watchedMovies';
 export const STORAGE_QUEUE_KEY = 'queueMovies';
+export const STORAGE_INPUT_KEY = 'inputSearchMovies';
