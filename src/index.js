27833ffd4d8558b1c75ee6c6c83bfc68a0d784a8
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-12-04 14:21:05 +0200

    add btn back to top

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
+import './js/btt'; // back to top
 import './js/helpers/data-format'; //это временно(Аня)
 
 import { fetchMovies } from './js/api-service-popular'; //запрос популярные фильмы(Наташа)
