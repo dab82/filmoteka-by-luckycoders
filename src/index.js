5d32b2867876963e6dd4e79df4b373e83d92e866
ITigor1985 <xromer85@gmail.com>
2021-12-04 14:16:33 +0200

    mobile modal

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './sass/main.scss';
 import './js/helpers/data-format'; //это временно(Аня)
+import 'material-icons/iconfont/material-icons.css';
 
 import { fetchMovies } from './js/api-service-popular'; //запрос популярные фильмы(Наташа)
---
+++ src/js/modal.js
@@ -0,0 +1,16 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelectorAll('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.forEach(card =>{
+        card.addEventListener('click', toggleModal);
+    });
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('visually-hidden');
+    }
+  })();
