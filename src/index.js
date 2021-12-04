1fc5a52c13e4867a8e4e9c86fe50dd1651f4ad21
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-12-04 16:19:05 +0200

    off btt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import 'material-icons/iconfont/material-icons.css';
-import './js/btt'; // back to top
+// import './js/btt'; // back to top
 import './js/helpers/data-format'; //это временно(Аня)
 import './js/modal.js';
 import { fetchMovies } from './js/api-service-popular'; //запрос популярные фильмы(Наташа)
