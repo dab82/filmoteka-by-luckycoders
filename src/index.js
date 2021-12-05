48e920cf06dd1fe723d695bd47a8732b7373bd64
NataliaStupka <NataliGluschenko@ukr.net>
2021-12-05 19:11:20 +0200

    отрисовка изначальной страницы с популярными фильмами

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import 'material-icons/iconfont/material-icons.css';
 // import './js/btt'; // back to top
 import './js/helpers/data-format'; //это временно(Аня)
 import './js/modal.js';
-import { fetchMovies } from './js/api-service-popular'; //запрос популярные фильмы(Наташа)
+
+import './js/popular-movies-render';
+
+
+
+
+
+
+
+
