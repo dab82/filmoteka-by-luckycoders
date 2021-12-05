fc045d88bd5ac3796a1efa8f2f0bd3654e83eca3
NataliaStupka <NataliGluschenko@ukr.net>
2021-12-05 19:17:59 +0200

    переставляет (в index.js) ./js/popular-movies-render  выше модалки

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import './sass/main.scss';
 import 'material-icons/iconfont/material-icons.css';
 // import './js/btt'; // back to top
 import './js/helpers/data-format'; //это временно(Аня)
+import './js/popular-movies-render';
 import './js/modal.js';
 
-import './js/popular-movies-render';
+
 
 
 
