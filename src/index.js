194b8e25448092f58b162db69e97c3d0e0470f78
Dmitriy Bondarenko <dabwork1982@gmail.com>
2021-12-07 01:24:51 +0200

    добавил черновой вариант модалки разработчиков

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@
 import 'material-icons/iconfont/material-icons.css';
 import 'animate.css';
 import './sass/main.scss';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 //IMPORT JS
 import './js/popular-movies-render';
 import './js/modal.js';
+import './js/team-modal';
 
 // import './js/btt'; // back to top
+// import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
