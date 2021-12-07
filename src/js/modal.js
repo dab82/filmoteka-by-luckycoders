96fea6ef7aa7bcfdd0f72d560e41af92cf2886e9
Igor <81867285+ITigor1985@users.noreply.github.com>
2021-12-07 11:13:11 +0200

    pravka

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,32 +1,33 @@
-const timerId = setInterval(() => {
-  if(localStorage === null){    
-    return
-  } else{
-    const refs = {
-      body: document.querySelector('body'),
-      openModal:  document.querySelectorAll('.list-card__link'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
-    };
-      
-  //console.log(refs.openModal)
-    refs.openModal.forEach(card =>{
-        card.addEventListener('click', toggleModal);
-    });
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal(event) {
-      event.preventDefault();
-      const filmsId = event.currentTarget.getAttribute('data-card-id');      
-      refs.modal.classList.toggle('visually-hidden');
-      refs.body.classList.toggle('bg-scrolling-element-when-modal-open');
-    }
-    clear()
-  }
-  
-  
-}, 1000);
+    
+      if (localStorage === null){
+        return
+      }else{
+        const refs = {
+          body: document.querySelector('body'),
+          link: document.querySelector('.list-card__link'),
+          openModal:  document.querySelector('.list-card'),
+          closeModalBtn: document.querySelector('[data-modal-close]'),
+          modal: document.querySelector('[data-modal]'),
+        };     
+        
+        refs.openModal.addEventListener("click", selectFilm);
+        refs.closeModalBtn.addEventListener('click', toggleModal); 
 
- function clear(){
-  clearInterval(timerId);
- }  
+    
+        function selectFilm(event){
+          if(event.target !== event.currenttarget){
+            console.log(event.target)
+            toggleModal(event)
+          }
+        }   
+
+        function toggleModal(event) {
+          event.preventDefault(); 
+          refs.modal.classList.toggle('visually-hidden');
+          refs.body.classList.toggle('bg-scrolling-element-when-modal-open');
+        }
+       
+      }
+      
+    
+    
