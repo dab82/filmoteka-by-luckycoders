666b043dedfb6191643c17c341876e84a8a12c54
Anna Samsonenko <foster.anna93@gmail.com>
2021-12-08 14:05:20 +0200

    add styles for cards and modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,89 +1,78 @@
-    import {renderModal} from './helpers/render-modal';
-    import {STORAGE_MAIN_KEY} from './common/keys';
-    import {STORAGE_SELECTED_KEY} from './common/keys'
-      
-      let data = [];
+import { renderModal } from './helpers/render-modal';
+import { STORAGE_MAIN_KEY } from './common/keys';
+import { STORAGE_SELECTED_KEY } from './common/keys';
 
-      if (localStorage.getItem(STORAGE_MAIN_KEY) === null){
-        
-      }else{
-        const refs = {
-          body: document.querySelector('body'),
-          link: document.querySelector('.list-card__link'),
-          openModal:  document.querySelector('.list-card'),          
-          modal: document.querySelector('[data-modal]'),
-          backdrop: document.querySelector('.backdrop'),
-        };     
-        
-        refs.openModal.addEventListener("click", selectFilm);
-        
-        
-    
-        function selectFilm(event){
-          if(event.target !== event.currentTarget){ 
-            
-            let element = findAncestor(event.target, '.list-card__link'); 
-            const elementId = element.getAttribute("data-card-id")
-            const dataLS = JSON.parse(localStorage.getItem(STORAGE_MAIN_KEY));
-             data = dataLS.filter(dat => dat.id == elementId)  
+let data = [];
 
-             setDataToLocalStorage(data);
-             renderModal(...data);
-             toggleModal(event)
+if (localStorage.getItem(STORAGE_MAIN_KEY) === null) {
+} else {
+  const refs = {
+    body: document.querySelector('body'),
+    link: document.querySelector('.list-card__link'),
+    openModal: document.querySelector('.list-card'),
+    modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
+  };
 
-             const closeModalBtn = document.querySelector('[data-modal-close]');
-             closeModalBtn.addEventListener('click', closeModal);            
-             refs.backdrop.addEventListener('click', closeBackdrop);
+  refs.openModal.addEventListener('click', selectFilm);
 
+  function selectFilm(event) {
+    if (event.target !== event.currentTarget) {
+      let element = findAncestor(event.target, '.list-card__link');
+      const elementId = element.getAttribute('data-card-id');
+      const dataLS = JSON.parse(localStorage.getItem(STORAGE_MAIN_KEY));
+      data = dataLS.filter(dat => dat.id == elementId);
 
-             function closeModal(){
-              closeModalBtn.classList.toggle('visually-hidden');
-              removeLocalStorage();
-              closeModalBtn.removeEventListener('click', closeModal); 
-            }
-          }
-        }   
-        
-        
-        function toggleModal() {
-          event.preventDefault();           
-          refs.modal.classList.toggle('visually-hidden');
-          refs.body.classList.toggle('bg-scrolling-element-when-modal-open');
-          document.addEventListener('keydown', closeModalEsc);
-        }
+      setDataToLocalStorage(data);
+      renderModal(...data);
+      toggleModal(event);
 
-        
-        function findAncestor(element, sel) {
-          while ((element = element.parentElement) && !((element.matches || element.matchesSelector).call(element,sel)));
-          return element;
-        };
+      const closeModalBtn = document.querySelector('[data-modal-close]');
+      closeModalBtn.addEventListener('click', closeModal);
+      refs.backdrop.addEventListener('click', closeBackdrop);
 
-        function setDataToLocalStorage(data) {          
-            localStorage.setItem(STORAGE_SELECTED_KEY, JSON.stringify(data));            
-        }
-        function removeLocalStorage(){
-          localStorage.removeItem(STORAGE_SELECTED_KEY);
-        }
-
-        function closeBackdrop(){
-          refs.backdrop.classList.toggle('visually-hidden');
-          refs.body.classList.toggle('bg-scrolling-element-when-modal-open');
-          removeLocalStorage();
-          document.removeEventListener('keydown', closeModalEsc);
-        }        
-
-        function closeModalEsc({key}){
-          
-          if(key ==="Escape" && !refs.modal.classList.contains("visually-hidden")){
-            removeLocalStorage();
-            toggleModal();
-            document.removeEventListener('keydown', closeModalEsc);
-          }
-
-        }
-        
-       
+      function closeModal() {
+        closeModalBtn.classList.toggle('is-hidden');
+        removeLocalStorage();
+        closeModalBtn.removeEventListener('click', closeModal);
       }
-      
-    
-    
+    }
+  }
+
+  function toggleModal(event) {
+    event.preventDefault();
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('bg-scrolling-element-when-modal-open');
+    document.addEventListener('keydown', closeModalEsc);
+  }
+
+  function findAncestor(element, sel) {
+    while (
+      (element = element.parentElement) &&
+      !(element.matches || element.matchesSelector).call(element, sel)
+    );
+    return element;
+  }
+
+  function setDataToLocalStorage(data) {
+    localStorage.setItem(STORAGE_SELECTED_KEY, JSON.stringify(data));
+  }
+  function removeLocalStorage() {
+    localStorage.removeItem(STORAGE_SELECTED_KEY);
+  }
+
+  function closeBackdrop() {
+    refs.backdrop.classList.toggle('is-hidden');
+    refs.body.classList.toggle('bg-scrolling-element-when-modal-open');
+    removeLocalStorage();
+    document.removeEventListener('keydown', closeModalEsc);
+  }
+
+  function closeModalEsc({ key }) {
+    if (key === 'Escape' && !refs.modal.classList.contains('is-hidden')) {
+      removeLocalStorage();
+      toggleModal();
+      document.removeEventListener('keydown', closeModalEsc);
+    }
+  }
+}
