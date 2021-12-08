01eaabfb3e039094d9f9bb3a82be9011fde437eb
Anna Samsonenko <foster.anna93@gmail.com>
2021-12-08 20:12:06 +0200

    add toogle

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,7 +1,43 @@
-toggleThemeBtm.onclick = () => {
-  document.body.classList.toggle('dark');
-  toggleThemeBtm.innerText = document.body.classList.contains('dark')
-    ? 'change a theme'
-    : 'change a theme';
-  localStorage.theme = document.body.className || 'light';
+// toggleThemeBtm.onclick = () => {
+//   document.body.classList.toggle('dark');
+//   toggleThemeBtm.innerText = document.body.classList.contains('dark')
+//     ? 'change a theme'
+//     : 'change a theme';
+//   localStorage.theme = document.body.className || 'light';
+// };
+
+const checkbox = document.querySelector('#theme-switch-toggle');
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
 };
+
+document.body.classList.add(Theme.LIGHT);
+
+checkbox.addEventListener('change', onCheckboxChange);
+
+function onCheckboxChange() {
+  document.body.classList.toggle(Theme.DARK);
+  document.body.classList.toggle(Theme.LIGHT);
+
+  setThemeInfoToLocalStorage();
+}
+
+function setThemeInfoToLocalStorage() {
+  if (document.body.classList.contains(Theme.LIGHT)) {
+    checkbox.setAttribute('checked', false);
+    localStorage.setItem('active-theme', Theme.LIGHT);
+  } else if (document.body.classList.contains(Theme.DARK)) {
+    checkbox.setAttribute('checked', true);
+    localStorage.setItem('active-theme', Theme.DARK);
+  }
+}
+
+getThemeInfoFromLocalStorage();
+
+function getThemeInfoFromLocalStorage() {
+  if (localStorage.getItem('active-theme') === Theme.DARK) {
+    onCheckboxChange();
+  }
+}
