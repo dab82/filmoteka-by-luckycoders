2305b6d441abfc8fbd8f05322c5a5a9893dd55fa
Svetlana Ovcharenko <lana.ovchar@gmail.com>
2021-12-08 19:39:47 +0200

    добавляет смену кнопок в library

--- src/js/common/refs.js
+++ src/js/common/refs.js
@@ -2,5 +2,12 @@ export const refs = {
   listContainer: document.querySelector('.list-card'),
   backdrop: document.querySelector('.backdrop'),
   pagination: document.querySelector('.tui-pagination'),
+  homeBackground: document.querySelector('.page-header'),
+  btnList: document.querySelector('.btn__list'),
+  input: document.querySelector('.search-form'),
+  homeLink: document.querySelector('.home-link'),
+  libLink: document.querySelector('.lib-link'),
+  watchedBtn: document.querySelector('.watched-btn'),
+  queueBtn: document.querySelector('.queue-btn'),
 };
 
