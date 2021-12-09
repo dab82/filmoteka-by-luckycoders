f6fe5832659f444418c280e641e19954da090722
shevchenko-andrey <andreases_mobimoz@ukr.net>
2021-12-09 02:06:23 +0200

    add feature btn-add-queue
    add feature btn-add-queue and bagfix api local storage

---
+++ src/js/btn-add-queue.js
@@ -0,0 +1,28 @@
+import { refs } from './common/refs';
+import { STORAGE_WATCHED_KEY, STORAGE_QUEUE_KEY } from './common/keys';
+import ApiLocalStorege from './api-local-storage';
+refs.backdrop.addEventListener('transitionend', onAddLisenerModalBtn);
+const onClickAddQueue = e => {
+  ApiLocalStorege.addCard(STORAGE_QUEUE_KEY);
+  e.target.removeEventListener('click', onClickAddQueue);
+  e.target.classList.remove('add-to-queue');
+  e.target.textContent = 'remove from queue';
+  e.target.classList.add("remove-from-queue'");
+  e.target.addEventListener('click', onClickRemoveQueue);
+};
+const onClickRemoveQueue = e => {
+  ApiLocalStorege.delCard(STORAGE_QUEUE_KEY);
+  e.target.removeEventListener('click', onClickRemoveQueue);
+  e.target.classList.remove('remove-from-queue');
+  e.target.textContent = 'add from queue';
+  e.target.classList.add('add-to-queue');
+  e.target.addEventListener('click', onClickAddQueue);
+};
+
+function onAddLisenerModalBtn() {
+  const modalBtnRefs = {
+    addQueue: document.querySelector('.add-to-queue'),
+    addWatched: document.querySelector('.add-to-watched'),
+  };
+  modalBtnRefs.addQueue.addEventListener('click', onClickAddQueue);
+}
