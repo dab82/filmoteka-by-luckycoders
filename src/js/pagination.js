0bd552e2eb1180cc85b1b535b6a7bc8b1dd9d967
Anna Samsonenko <foster.anna93@gmail.com>
2021-12-09 12:02:38 +0200

    add styles for pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -5,6 +5,10 @@ import { setDataToStorageForHome } from './popular-movies-render';
 import { renderListCard } from './helpers/render-list-card';
 import Pagination from 'tui-pagination';
 import { refs } from './common/refs';
+import sprite from '../images/sprite.svg';
+
+const arrowIcon = `${sprite}#icon-arrow`;
+const dotsIcon = `${sprite}#icon-dots`;
 
 export const paginationSettings = {
   startPage: 1,
@@ -26,7 +30,7 @@ export const initPagination = ({ page, itemsPerPage, totalItems }) => {
       currentPage: '<a href="#" class="tui-page-btn tui-is-selected">{{page}}</a>',
       moveButton:
         '<a href="#" class="tui-page-btn tui-{{type}} hide-{{type}}">' +
-        '<span class="tui-ico-{{type}}">{{type}}</span>' +
+        `<svg class="tui-ico-{{type}}" width="16" height="16"><use href="${arrowIcon}-{{type}}"></use></svg>` +
         '</a>',
       disabledMoveButton:
         '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
@@ -34,7 +38,7 @@ export const initPagination = ({ page, itemsPerPage, totalItems }) => {
         '</span>',
       moreButton:
         '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip change-{{type}}">' +
-        '<span class="tui-ico-ellip">...</span>' +
+        `<svg class="tui-ico-ellip" width="14" height="14"><use href="${dotsIcon}"></use></svg>` +
         '</a>',
     },
   };
