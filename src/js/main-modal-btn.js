1a870d2897905f6288b2c2ad77e5ec96795b3d42
shevchenko-andrey <andreases_mobimoz@ukr.net>
2021-12-10 02:34:15 +0200

    add full logic modal button

--- src/js/main-modal-btn.js
+++ src/js/main-modal-btn.js
@@ -1,7 +1,7 @@
 import { refs } from './common/refs';
 import { STORAGE_QUEUE_KEY, STORAGE_WATCHED_KEY } from './common/keys';
-import { onClickAddQueue, onClickRemoveQueue } from './btn-add-queue';
-import { onClickAddWatched, onClickRemoveWatched } from './btn-add-watched';
+import { addMod, removeMod } from './modal-btn-mods';
+import ApiLocalStorege from './local-storage-library';
 
 refs.backdrop.addEventListener('transitionend', onAddLisenerModalBtn);
 
@@ -10,22 +10,38 @@ function onAddLisenerModalBtn() {
     addQueue: document.querySelector('[data-modal-queue]'),
     addWatched: document.querySelector('[data-modal-watched]'),
   };
-  if (!refs.backdrop.classList.contains('is-hidden')) {
+  if (refs.backdrop.classList.contains('is-hidden')) {
     btnRefs.addQueue.removeEventListener('click', onClickBtnQueue);
     btnRefs.addWatched.removeEventListener('click', onClickBtnWatched);
-    console.log('мы вышли');
     return;
   }
-
+  const filmToQueue = ApiLocalStorege.searchFilm(STORAGE_QUEUE_KEY);
+  const filmToWatched = ApiLocalStorege.searchFilm(STORAGE_WATCHED_KEY);
+  if (filmToQueue) {
+    removeMod(btnRefs.addQueue, 'queue');
+  }
+  if (filmToWatched) {
+    removeMod(btnRefs.addWatched, 'watched');
+  }
   btnRefs.addQueue.addEventListener('click', onClickBtnQueue);
-  btnRefs.addQueue.addEventListener('click', onClickBtnWatched);
+  btnRefs.addWatched.addEventListener('click', onClickBtnWatched);
 }
 
 function onClickBtnQueue(e) {
-  onClickAddQueue(STORAGE_WATCHED_KEY);
-  searchFilm(STORAGE_WATCHED_KEY);
-  console.log('searchFilm(STORAGE_WATCHED_KEY)', searchFilm(STORAGE_WATCHED_KEY));
+  if (e.target.classList.contains('add-to-queue')) {
+    ApiLocalStorege.addCard(STORAGE_QUEUE_KEY);
+    removeMod(e.target, 'queue');
+  } else if (e.target.classList.contains('remove-from-queue')) {
+    ApiLocalStorege.delCard(STORAGE_QUEUE_KEY);
+    addMod(e.target, 'queue');
+  }
 }
 function onClickBtnWatched(e) {
-  searchFilm(STORAGE_QUEUE_KEY);
+  if (e.target.classList.contains('add-to-watched')) {
+    ApiLocalStorege.addCard(STORAGE_WATCHED_KEY);
+    removeMod(e.target, 'watched');
+  } else if (e.target.classList.contains('remove-from-watched')) {
+    ApiLocalStorege.delCard(STORAGE_WATCHED_KEY);
+    addMod(e.target, 'watched');
+  }
 }
