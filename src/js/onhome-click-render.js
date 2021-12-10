9d99ef60510ed01b0af697d121ceb16067a2376e
Anna Samsonenko <foster.anna93@gmail.com>
2021-12-10 13:27:42 +0200

    add pagination enabled on home click

--- src/js/onhome-click-render.js
+++ src/js/onhome-click-render.js
@@ -1,6 +1,8 @@
 import { refs } from './common/refs';
 import { STORAGE_HOME_KEY, STORAGE_MAIN_KEY } from './common/keys';
 import { renderListCard } from './helpers/render-list-card';
+import { paginationSettings } from './pagination';
+import { HOME_SEARCH_TYPE, TOP_SEARCH_TYPE } from './common/search-types';
 
 refs.onLogo.addEventListener('click', renderCards);
 refs.homeLink.addEventListener('click', renderCards);
@@ -8,6 +10,8 @@ refs.homeLink.addEventListener('click', renderCards);
 function renderCards(e) {
   e.preventDefault();
   const storageData = JSON.parse(localStorage.getItem(STORAGE_HOME_KEY));
+  paginationSettings.searchType = HOME_SEARCH_TYPE;
+  paginationSettings.pagination.reset(paginationSettings.totalItemsHome);
   renderListCard(storageData);
   localStorage.setItem(STORAGE_MAIN_KEY, JSON.stringify(storageData));
 }
