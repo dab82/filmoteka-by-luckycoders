14e29f6a6b27a99620d60e8512b75a2aa4563d39
Anna Samsonenko <foster.anna93@gmail.com>
2021-12-11 18:21:43 +0200

    fixed first page render for modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -8,113 +8,111 @@ import ApiLocalStorege from './local-storage-library';
 
 let data = [];
 
-if (localStorage.getItem(STORAGE_MAIN_KEY) === null) {
-} else {
-  const refs = {
-    body: document.querySelector('body'),
-    link: document.querySelector('.list-card__link'),
-    openModal: document.querySelector('.list-card'),
-    modal: document.querySelector('[data-modal]'),
-  };
+const refs = {
+  body: document.querySelector('body'),
+  link: document.querySelector('.list-card__link'),
+  openModal: document.querySelector('.list-card'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  refs.openModal.addEventListener('click', selectFilm);
+refs.openModal.addEventListener('click', selectFilm);
 
-  function selectFilm(event) {
-    if (event.target !== event.currentTarget) {
-      let element = findAncestor(event.target, '.list-card__link');
-      const elementId = element.getAttribute('data-card-id');
+function selectFilm(event) {
+  if (event.target !== event.currentTarget) {
+    let element = findAncestor(event.target, '.list-card__link');
+    const elementId = element.getAttribute('data-card-id');
 
-      let dataLS = null;
+    let dataLS = null;
 
-      try {
-        dataLS = JSON.parse(localStorage.getItem(STORAGE_MAIN_KEY));
-      } catch {
-        return;
-      }
+    try {
+      dataLS = JSON.parse(localStorage.getItem(STORAGE_MAIN_KEY));
+    } catch {
+      console.log(error);
+      return;
+    }
 
-      data = dataLS.filter(dat => dat.id == elementId);
+    data = dataLS.filter(dat => dat.id == elementId);
 
-      // if(filmToQueue){
-      //   renderModal(...filmToQueue);
-      //   toggleModal(event);
-      // }else{
-      setDataToLocalStorage(data);
-      renderModal(...data);
-      const btnRefs = {
-        addQueue: document.querySelector('[data-modal-queue]'),
-        addWatched: document.querySelector('[data-modal-watched]'),
-      };
-      const filmToQueue = ApiLocalStorege.searchFilm(STORAGE_QUEUE_KEY);
-      const filmToWatched = ApiLocalStorege.searchFilm(STORAGE_WATCHED_KEY);
-      if (filmToQueue) {
-        removeMod(btnRefs.addQueue, 'queue');
-      }
-      if (filmToWatched) {
-        removeMod(btnRefs.addWatched, 'watched');
-      }
-      toggleModal(event);
-      // }
+    // if(filmToQueue){
+    //   renderModal(...filmToQueue);
+    //   toggleModal(event);
+    // }else{
+    setDataToLocalStorage(data);
+    renderModal(...data);
+    const btnRefs = {
+      addQueue: document.querySelector('[data-modal-queue]'),
+      addWatched: document.querySelector('[data-modal-watched]'),
+    };
+    const filmToQueue = ApiLocalStorege.searchFilm(STORAGE_QUEUE_KEY);
+    const filmToWatched = ApiLocalStorege.searchFilm(STORAGE_WATCHED_KEY);
+    if (filmToQueue) {
+      removeMod(btnRefs.addQueue, 'queue');
+    }
+    if (filmToWatched) {
+      removeMod(btnRefs.addWatched, 'watched');
+    }
+    toggleModal(event);
+    // }
 
-      // setDataToLocalStorage(data);
-      // renderModal(...data);
-      // toggleModal(event);
+    // setDataToLocalStorage(data);
+    // renderModal(...data);
+    // toggleModal(event);
 
-      const closeModalBtn = document.querySelector('[data-modal-close]');
-      closeModalBtn.addEventListener('click', closeModal);
-      refs.modal.addEventListener('click', closeBackdrop);
+    const closeModalBtn = document.querySelector('[data-modal-close]');
+    closeModalBtn.addEventListener('click', closeModal);
+    refs.modal.addEventListener('click', closeBackdrop);
 
-      function closeModal() {
+    function closeModal() {
+      removeClassIsShown();
+      removeLocalStorage();
+      closeModalBtn.removeEventListener('click', closeModal);
+    }
+
+    function toggleModal(event) {
+      event.preventDefault();
+      addClassIsShown();
+      document.addEventListener('keydown', closeModalEsc);
+    }
+
+    function findAncestor(element, sel) {
+      while (
+        (element = element.parentElement) &&
+        !(element.matches || element.matchesSelector).call(element, sel)
+      );
+      return element;
+    }
+
+    function setDataToLocalStorage(data) {
+      localStorage.setItem(STORAGE_SELECTED_KEY, JSON.stringify(data));
+    }
+    function removeLocalStorage() {
+      localStorage.removeItem(STORAGE_SELECTED_KEY);
+    }
+
+    function closeBackdrop(event) {
+      if (event.target === refs.modal) {
         removeClassIsShown();
+        document.removeEventListener('keydown', closeModalEsc);
+        refs.modal.removeEventListener('click', closeBackdrop);
+      }
+    }
+
+    function closeModalEsc({ key }) {
+      if (key === 'Escape' && !refs.modal.classList.contains('is-hidden')) {
         removeLocalStorage();
+        document.removeEventListener('keydown', closeModalEsc);
+        removeClassIsShown();
         closeModalBtn.removeEventListener('click', closeModal);
+        refs.modal.removeEventListener('click', closeBackdrop);
       }
-
-      function toggleModal(event) {
-        event.preventDefault();
-        addClassIsShown();
-        document.addEventListener('keydown', closeModalEsc);
-      }
-
-      function findAncestor(element, sel) {
-        while (
-          (element = element.parentElement) &&
-          !(element.matches || element.matchesSelector).call(element, sel)
-        );
-        return element;
-      }
-
-      function setDataToLocalStorage(data) {
-        localStorage.setItem(STORAGE_SELECTED_KEY, JSON.stringify(data));
-      }
-      function removeLocalStorage() {
-        localStorage.removeItem(STORAGE_SELECTED_KEY);
-      }
-
-      function closeBackdrop(event) {
-        if (event.target === refs.modal) {
-          removeClassIsShown();
-          document.removeEventListener('keydown', closeModalEsc);
-          refs.modal.removeEventListener('click', closeBackdrop);
-        }
-      }
-
-      function closeModalEsc({ key }) {
-        if (key === 'Escape' && !refs.modal.classList.contains('is-hidden')) {
-          removeLocalStorage();
-          document.removeEventListener('keydown', closeModalEsc);
-          removeClassIsShown();
-          closeModalBtn.removeEventListener('click', closeModal);
-          refs.modal.removeEventListener('click', closeBackdrop);
-        }
-      }
-      function removeClassIsShown() {
-        refs.modal.classList.remove('is-shown');
-        refs.body.classList.remove('bg-scrolling-element-when-modal-open');
-      }
-      function addClassIsShown() {
-        refs.modal.classList.add('is-shown');
-        refs.body.classList.add('bg-scrolling-element-when-modal-open');
-      }
+    }
+    function removeClassIsShown() {
+      refs.modal.classList.remove('is-shown');
+      refs.body.classList.remove('bg-scrolling-element-when-modal-open');
+    }
+    function addClassIsShown() {
+      refs.modal.classList.add('is-shown');
+      refs.body.classList.add('bg-scrolling-element-when-modal-open');
     }
   }
 }
